cd4cbfb6120a4093fc93346672286c3fb8462871
bartderijk <derijkbart@gmail.com>
2020-01-04 11:10:06 +0100

    Added the ObjectDetails component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <TopHero/>
+    <ObjectDetails/>
   </div>
 </template>
 
 <script>
 import TopHero from './components/TopHero.vue';
+import ObjectDetails from './components/ObjectDetails.vue';
 
 export default {
   name: 'app',
   components: {
     TopHero,
+    ObjectDetails,
   },
 };
 </script>
---
+++ src/components/ObjectDetails.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <section class="level">
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Bouwjaar</p>
+          <p class="title">1995</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Wonen</p>
+          <p class="title">120m²</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Aantal kamers</p>
+          <p class="title">5</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Perceel</p>
+          <p class="title">140m²</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ObjectDetails',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .container {
+        margin-top: 16px;
+    }
+</style>
