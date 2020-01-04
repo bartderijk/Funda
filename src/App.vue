779032ed3bce4941e0e1b57b096aedbe4c96e7a7
bartderijk <derijkbart@gmail.com>
2020-01-04 10:32:53 +0100

    Added TopHero component, also got rid of some of the default Vue CLI templating

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopHero msg="Lorem ipsum dolor sitamet"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import TopHero from './components/TopHero.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    TopHero,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/TopHero.vue
@@ -0,0 +1,21 @@
+<template>
+  <section  class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">{{ msg }}</h1>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'TopHero',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
