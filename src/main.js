f961ac798872227f10a507fd346cefe5ca59ee14
bartderijk <derijkbart@gmail.com>
2020-01-05 17:12:58 +0100

    Got rid of fontawesome via cdn, now managed via npm

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import './assets/sass/styles.scss';
+import '../node_modules/@fortawesome/fontawesome-free/js/all.min';
 
 Vue.config.productionTip = false;
 
